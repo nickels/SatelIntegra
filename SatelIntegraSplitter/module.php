d4ff423ee503bc9849f9344459acc2932375e398
Nicolaas Nijman <nicolaas@nijman.co>
2020-02-17 21:47:30 +0100

    test

---
+++ SatelIntegraSplitter/module.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+class SatelIntegraSplitter extends IPSModule
+{
+    // Überschreibt die interne IPS_Create($id) Funktion
+    public function Create()
+    {
+        // Diese Zeile nicht löschen.
+        parent::Create();
+
+    }
+
+    // Überschreibt die intere IPS_ApplyChanges($id) Funktion
+    public function ApplyChanges()
+    {
+        // Diese Zeile nicht löschen
+        parent::ApplyChanges();
+    }
+
+    /**
+     * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
+     * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
+     *
+     * ABC_MeineErsteEigeneFunktion($id);
+     *
+     */
+    public function MeineErsteEigeneFunktion()
+    {
+        // Selbsterstellter Code
+    }
+}
